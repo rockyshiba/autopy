2383ef2ba840670be010ccb498d23b0a7440d9a4
Lee <leejsitu@gmail.com>
2020-09-02 19:28:38 +0900

    emojis.py init

---
+++ Chapter 7/emojis.py
@@ -0,0 +1,15 @@
+# good link for regex: https://www.regular-expressions.info/
+# for python regex: https://pythex.org/.
+# good hint here to figure out how to find emojis: https://gist.github.com/Alex-Just/e86110836f3f93fe7932290526529cd1 
+
+import re
+
+# A string that is exactly 'abc'
+
+# A number that is exactly 123
+
+# Any singular letter
+
+# Any singular digit
+
+# Two characters, as long as its a letter followed by a digit eg. a1, b1, c2
